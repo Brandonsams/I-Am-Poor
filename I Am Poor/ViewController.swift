768f9f4160ef0d80d3c1193828dd86feaf81b971
brandonsams <brandonkeithsams@gmail.com>
2022-08-17 22:08:03 -0600

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Brandon Sams on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
